ca078bfd4e45cce0a74bb8ba0102df33b15227a0
hugosenari <hugosenari@gmail.com>
2022-03-12 02:18:20 -0300

    chore: re index oop_utils

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Macro for building OOP class hierarchies based on closure methods.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."oop_utils-master".url = "path:./master";
+  inputs."oop_utils-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."oop_utils-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Macro for building OOP class hierarchies based on closure methods.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-oop_utils-master.flake = false;
+  inputs.src-oop_utils-master.owner = "bluenote10";
+  inputs.src-oop_utils-master.ref   = "refs/heads/master";
+  inputs.src-oop_utils-master.repo  = "oop_utils";
+  inputs.src-oop_utils-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-oop_utils-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-oop_utils-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
